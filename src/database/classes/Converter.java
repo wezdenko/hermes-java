7dd003e0e663c05dca30c63358012e2c52c296d3
Mateusz Wezdenko <mateuszwez@op.pl>
2021-01-04 14:09:00 +0100

    fixed problems during merging

--- src/database/classes/Converter.java
+++ src/database/classes/Converter.java
@@ -1,4 +1,6 @@
-git package database.classes;
+package database.classes;
+
+import java.sql.Date;
 
 public class Converter {
     
@@ -33,4 +35,58 @@ public class Converter {
             return 0.0;
         }
     }
+
+    public static String DoubleToString(double i){
+        try{
+        return String.valueOf(i);
+        }
+        catch(Exception e){
+            AlertBox.display("Exception", "Wrong data type");
+            return "0";
+        }
+    }
+
+    public static Date StringToDate (String s){
+        try{
+        Date decode = Date.valueOf(s);
+        return decode;
+        }
+        catch(Exception e){
+            AlertBox.display("Exception", "Wrong data type\nyyyy-[m]m-[d]d");
+            return new Date(0, 0, 0);
+        }
+    }
+
+    public static String DateToString(Date date){
+        try{
+        return date.toString();
+        }
+        catch(Exception e){
+            AlertBox.display("Exception", "Wrong data type");
+            return "0";
+        }
+    }
+
+
+public static Long StringToLong (String s){
+    try{
+    Long decode = Long.parseLong(s);
+    return decode;
+    }
+    catch(NumberFormatException e){
+        AlertBox.display("Exception", "Wrong data type");
+        return 0L;
+    }
+}
+
+public static String LongToString(long i){
+    try{
+    return String.valueOf(i);
+    }
+    catch(Exception e){
+        AlertBox.display("Exception", "Wrong data type");
+        return "0";
+    }
+}
+
 }
--- src/database/classes/Parcel.java
+++ src/database/classes/Parcel.java
@@ -64,8 +64,16 @@ public class Parcel {
         this.cost = cost;
     }
 
+    public String getCost_S() {
+        return Converter.DoubleToString(this.cost);
+    }
+    
     public double getWeight() {
         return weight;
+    }
+
+    public String getWeight_S() {
+        return Converter.DoubleToString(this.weight);
     }
 
     public void setWeight(double weight) {
@@ -76,12 +84,20 @@ public class Parcel {
         return length;
     }
 
+    public String getLength_S() {
+        return Converter.DoubleToString(this.length);
+    }
+
     public void setLength(double length) {
         this.length = length;
     }
 
     public double getWidth() {
         return width;
+    }
+
+    public String getWidth_S() {
+        return Converter.DoubleToString(this.width);
     }
 
     public void setWidth(double width) {
@@ -92,12 +108,20 @@ public class Parcel {
         return height;
     }
 
+    public String getHeight_S() {
+        return Converter.DoubleToString(this.height);
+    }
+
     public void setHeight(double height) {
         this.height = height;
     }
 
     public int getCode() {
         return code;
+    }
+
+    public String getCode_S() {
+        return Converter.IntToString(this.code);
     }
 
     public void setCode(int code) {
@@ -120,12 +144,20 @@ public class Parcel {
         return collectionPointID;
     }
 
+    public String getCollectionPointID_S() {
+        return Converter.IntToString(this.collectionPointID);
+    }
+
     public void setCollectionPointID(int collectionPointID) {
         this.collectionPointID = collectionPointID;
     }
 
     public int getDepartmentID() {
         return departmentID;
+    }
+
+    public String getDepartmentID_S() {
+        return Converter.IntToString(this.departmentID);
     }
 
     public void setDepartmentID(int departmentID) {
@@ -148,27 +180,172 @@ public class Parcel {
         this.sender = sender;
     }
 
-    public String getSenderName() {
+    public String getSName() {
         return this.sender.getName();
     }
-    public String getReceiverName() {
-        return this.receiver.getName();
-    }
 
-    public void setSenderName(String name) {
+    public void setSName(String name) {
         this.sender.setName(name);
     }
 
-    public void setReceiverName(String name) {
+    public String getSSurname() {
+        return this.sender.getSurname();
+    }
+
+    public String getSEmail() {
+        return this.sender.getEmail();
+    }
+
+    public void setSEmail(String email) {
+        this.sender.setEmail(email);
+    }
+
+    public String getSPhoneNumber() {
+        return this.sender.getPhoneNumber();
+    }
+
+    public void setSPhoneNumber(String number) {
+        this.sender.setPhoneNumber(number);
+    }
+
+    public void setSSurname(String surname) {
+        this.sender.setSurname(surname);
+    }
+
+    public String getRName() {
+        return this.receiver.getName();
+    }
+
+    public void setRName(String name) {
         this.receiver.setName(name);
+    }
+
+    public String getRSurname() {
+        return this.receiver.getSurname();
+    }
+
+    public void setRSurname(String surname) {
+        this.receiver.setSurname(surname);
+    }
+
+    public String getREmail() {
+        return this.receiver.getEmail();
+    }
+
+    public void setREmail(String email) {
+        this.receiver.setEmail(email);
+    }
+
+    public String getRPhoneNumber() {
+        return this.receiver.getPhoneNumber();
+    }
+
+    public void setRPhoneNumber(String number) {
+        this.receiver.setPhoneNumber(number);
+    }
+
+    public String getSenderAddress() {
+        return this.sender.getClientAddress().getFullAddress();
+    }
+
+    public String getSAddress_street() {
+        return this.sender.getClientAddress().getStreet();
+    }
+
+    public void setSAddress_street(String street) {
+        this.sender.getClientAddress().setStreet(street);
+    }
+
+    public String getSAddress_houseNumber() {
+        return Converter.IntToString(this.sender.getClientAddress().getHouseNumber());
+    }
+
+    public void setSAddress_houseNumber(int number) {
+        this.sender.getClientAddress().setHouseNumber(number);
+    }
+
+    public String getSAddress_apartmentNumber() {
+        return Converter.IntToString(this.sender.getClientAddress().getApartmentNumber());
+    }
+
+    public void setSAddress_apartmentNumber(int number) {
+        this.sender.getClientAddress().setApartmentNumber(number);
+    }
+
+    public String getSAddress_city() {
+        return this.sender.getClientAddress().getCity();
+    }
+
+    public void setSAddress_city(String city) {
+        this.sender.getClientAddress().setCity(city);
+    }
+
+    public String getSAddress_postalCode() {
+        return this.sender.getClientAddress().getPostalCode();
+    }
+
+    public void setSAddress_postalCode(String code) {
+        this.sender.getClientAddress().setPostalCode(code);
+    }
+
+    public String getSAddress_countryID() {
+        return Converter.IntToString(this.sender.getClientAddress().getCountryID());
+    }
+
+    public void setSAddress_countryID(int id) {
+        this.sender.getClientAddress().setCountryID(id);
     }
 
     public String getReceiverAddress() {
         return this.receiver.getClientAddress().getFullAddress();
     }
 
-    public String getSenderAddress() {
-        return this.sender.getClientAddress().getFullAddress();
+    public String getRAddress_street() {
+        return this.receiver.getClientAddress().getStreet();
+    }
+
+    public void setRAddress_street(String street) {
+        this.receiver.getClientAddress().setStreet(street);
+    }
+
+    public String getRAddress_houseNumber() {
+        return Converter.IntToString(this.receiver.getClientAddress().getHouseNumber());
+    }
+
+    public void setRAddress_houseNumber(int number) {
+        this.receiver.getClientAddress().setHouseNumber(number);
+    }
+
+    public String getRAddress_apartmentNumber() {
+        return Converter.IntToString(this.receiver.getClientAddress().getApartmentNumber());
+    }
+
+    public void setRAddress_apartmentNumber(int number) {
+        this.receiver.getClientAddress().setApartmentNumber(number);
+    }
+
+    public String getRAddress_city() {
+        return this.receiver.getClientAddress().getCity();
+    }
+
+    public void setRAddress_city(String city) {
+        this.receiver.getClientAddress().setCity(city);
+    }
+
+    public String getRAddress_postalCode() {
+        return this.receiver.getClientAddress().getPostalCode();
+    }
+
+    public void setRAddress_postalCode(String code) {
+        this.receiver.getClientAddress().setPostalCode(code);
+    }
+
+    public String getRAddress_countryID() {
+        return Converter.IntToString(this.receiver.getClientAddress().getCountryID());
+    }
+
+    public void setRAddress_countryID(int id) {
+        this.receiver.getClientAddress().setCountryID(id);
     }
 
     public String getSenderFullName() {
@@ -179,4 +356,3 @@ public class Parcel {
         return this.receiver.getName() + " " + this.receiver.getSurname();
     }
 }
-
