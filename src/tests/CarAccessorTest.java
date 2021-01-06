e6ff32834adf07f64f8f25a72e9d0463dff920a8
Mateusz Wezdenko <mateuszwez@op.pl>
2021-01-06 20:52:18 +0100

    tests for update car data

--- src/tests/CarAccessorTest.java
+++ src/tests/CarAccessorTest.java
@@ -9,6 +9,7 @@ import database.classes.Car;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Random;
 
 public class CarAccessorTest {
     
@@ -25,6 +26,37 @@ public class CarAccessorTest {
         assertEquals(car.getModel(), "Chevy Silverado");
         assertEquals(car.getDepartmentID(), 4);
         assertEquals(car.getEmployeeID(), 4);
+
+        Database.closeConnection(con);
+    }
+
+    @Test
+    public void updateCar() throws SQLException {
+
+        Connection con = Database.getConnection();
+        CarDataAccessor carAccessor = new CarDataAccessor(con);
+
+        Car car = carAccessor.getCar("select * from cars where cars_id = 5");
+
+        // generating random data
+        String plates[] = {"ABC-1234", "BDF-5678", "JKL-9876"};
+        String models[] = {"Chevy Silverado", "Honda Odyssey", "BMW 328i"};
+        int randomId[] = {1, 2, 3};
+
+        int rnd = new Random().nextInt(3);
+
+        // setting random data
+        car.setPlates(plates[rnd]);
+        car.setModel(models[rnd]);
+        car.setDepartmentID(randomId[rnd]);
+
+        // tested function
+        carAccessor.updateCar(car);
+
+        // tests
+        assertEquals(car.getPlates(), plates[rnd]);
+        assertEquals(car.getModel(), models[rnd]);
+        assertEquals(car.getDepartmentID(), randomId[rnd]);
 
         Database.closeConnection(con);
     }
